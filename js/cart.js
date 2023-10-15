2b7caaa1fd3ee4e4fd1c63299d431e6314873071
unknown <thanh.van19062004@gmail.com>
2023-10-15 11:52:18 +0700

    first commit

--- js/cart.js
+++ js/cart.js
@@ -1,39 +1,43 @@
-const cart = JSON.parse(localStorage.getItem('cart')) || [];
+const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-function calculateTotalPrice() {
-    let total = cart.reduce((acc, curr) => acc + curr.quantity * curr.price, 0);
-    document.getElementById('totalPrice').innerHTML = total.toLocaleString();
+function tinhtongtien(){
+  let total = cart.reduce((acc,curr) => acc + curr.quantity* curr.price,0)
+  document.getElementById("totall").innerHTML = total;
 }
+
 
 function renderCart() {
-    let element = '';
-    for (let i = 0; i < cart.length; i++) {
-        element += `
-        <section class="cart">
-        <td>${cart[i].id}</td>
-        <div class="cart-item">
-          <img
-            src="${cart[i].img}"
-            alt="Sản phẩm 1"
-          />
-          <td>${cart[i].quantity}</td>
-          <div class="product-details">
-            <h2>${cart[i].name}</h2>
-            <p style="color: red">${(cart[i].quantity * cart[i].price).toLocaleString()}đ</p>
-            <div class="quantity">
-              <button class="quantity-button minus">-</button>
-              <span class="quantity-text">${cart[i].quantity}</span>
-              <button class="quantity-button plus">+</button>
+  let element = "";
+  for (let i = 0; i < cart.length; i++) {
+    element += `
+
+
+
+
+          <section class="cart">
+          <div class="cart-item">
+            <img
+              src=${cart[i].image}
+              alt="Sản phẩm 1"
+            />
+            <div class="product-details">
+              <h2>${cart[i].name}</h2>
+              <p style="color: red">${cart[i].price.toLocaleString()} VND</p>
+              <div class="quantity">
+                <button class="quantity-button minus">-</button>
+                <span class="quantity-text">${ cart[i].quantity}</span>
+                <button class="quantity-button plus">+</button>
+              </div>
             </div>
+            <button class="remove-button">Xóa</button>
           </div>
-          <button class="remove-button">Xóa</button>
-        </div>
-      </section>
-        `;
-    }
-    document.getElementById('tb-body').innerHTML = element;
-    calculateTotalPrice();
+        </section>
+  
+    `;
+  }
+
+  document.getElementById("cartlist").innerHTML = element;
 }
 
-// Gọi hàm để hiển thị giỏ hàng
 renderCart();
+tinhtongtien();
